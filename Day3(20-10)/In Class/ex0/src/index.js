782646cef37d053a7d5bd1b5a9aeb78a62c624a4
tangquochung <19521583@gm.uit.edu.vn>
2022-10-20 14:37:56 +0700

    bt3

--- Day3(20-10)/In Class/ex0/src/index.js
+++ Day3(20-10)/In Class/ex0/src/index.js
@@ -1,3 +1,5 @@
+//ex0
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +17,47 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+//ex0.1
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <button />
+    <code />
+    <input />
+    <label />
+    <p />
+    <pre />
+    <select />
+    <table />
+    <ul />
+  </div>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+*/
+
+//ex0.3
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MySection from "./Ex0.3-MySection";
+import MyButton from "./Ex0.3-MyButton";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MySection>
+        <MyButton>My Button Text</MyButton>
+  </MySection>
+);
